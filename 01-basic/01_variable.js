f5b1f23c39c190ae67a8f63d38002c7be1ae49ce
Zeelani <65568348+afrozzeelani@users.noreply.github.com>
2024-03-18 07:07:28 +0000

    variables

--- 01-basic/01_variable.js
+++ 01-basic/01_variable.js
@@ -2,7 +2,7 @@ const accountId= 12345
 let accountEmail = "test1@gmail.com"
 var accountPasswor = "123456"
 accountCity = "noida"
-let accountState; //only variable define and value not define then undefined 
+let accountState; //only variable define but value not define then undefined 
 
 // accountId = 2 not allowed 
 /*
