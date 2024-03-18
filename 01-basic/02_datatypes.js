b0a97cefd62bac6e64fe31c41f4e3a0b5bdb38d0
Zeelani <65568348+afrozzeelani@users.noreply.github.com>
2024-03-18 09:10:05 +0000

    datatypes

---
+++ 01-basic/02_datatypes.js
@@ -0,0 +1,29 @@
+"use Strict"
+
+//alert(3+3)   we are using node.js not browser
+
+console.log(3+9); 
+console.log("afrozzeelani");
+
+
+let name = "afroz"
+let age = 24
+let isLoggedIn = false
+
+ 
+// number => 2 to power 53
+// biginit =>
+// string => ""
+// boolean => true/false
+// null =>   standalone value
+// undefined =>
+// symbole => unique
+
+
+
+
+
+console.log(typeof null);   //object 
+console.log(typeof undefined); //undefined
+
+
